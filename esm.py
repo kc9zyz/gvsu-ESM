915bf70cadc6c73477bec17a0fa086fadd6d8c89
Billy Neuson <bneuson@gmail.com>
2016-06-02 00:22:28 -0400

    Added SCPI commands to DC load control

--- esm.py
+++ esm.py
@@ -1,10 +1,21 @@
 import esmPrint as esmPrint
 from esmPrint import esmPrintSource as ps
 import esmSerial
+import esmDCLoad
 
 def main():
     dprint = esmPrint.esmPrint()
+
     serial = esmSerial.esmSerial()
-    serial.init(dprint)
+    dprint = esmPrint.esmPrint()
+    dprint.init()
+    serPorts = #[(esmSerial.esmSerialPorts.uiMicro,"/dev/ptyp1", serCallback)]
+#            (esmSerial.esmSerialPorts.panelMicro,"/dev/ptyp2", serCallback),
+[            (esmSerial.esmSerialPorts.electronicLoad,"/dev/ptyp3", esmDCLoad.callback)]
+#            (esmSerial.esmSerialPorts.stringInverter,"/dev/ptyp4", serCallback)]
+    serial.init(dprint,serPorts))
+    esmDCLoad.trackMPPT(serial,dprint)
+
+
 if __name__ == "__main__":
     main()
--- esmDCLoad.py
+++ esmDCLoad.py
@@ -9,19 +9,41 @@ class powerPointTrack:
 
     def all(self):
         return [self.voltage,self.current,self.power]
+def callback(byte):
+    # Store message until newline
+    message +=byte
 
 def setCurrent(serial, current):
     # Send the command to set the current of the load
-    msg = "CURR" + str(current)
+    msg = "CURR" + str(current) + "mA\n"
     if serial.sendSerial(esmSerial.esmSerialPorts.electronicLoad,msg) == len(msg):
         return False
     else:
         return True
+def getPower(serial):
+    # Send the command to get the power
+    msg = "MEAS:POW?\n"
+    serial.sendSerial(esmSerial.esmSerialPorts.electronicLoad,msg)
+
+def prepareDevice(serial):
+    # Disable Input
+    msg = "INP OFF"
+    serial.sendSerial(esmSerial.esmSerialPorts.electronicLoad,msg)
+    # Set to CCH mode
+    msg = "MODE CCH"
+    serial.sendSerial(esmSerial.esmSerialPorts.electronicLoad,msg)
+    # Enable input
+    msg = "INP ON"
+    serial.sendSerial(esmSerial.esmSerialPorts.electronicLoad,msg)
 
 def trackMPPT(serial, Dprint):
+
+    # Set the parameters to 0 
     current = 0
     voltage = 0
     power = 0
+
+    #Verify that print is setup
     if Dprint == None:
         print('ERR: DPrint not given, falling back')
         return (True,0)
@@ -30,11 +52,15 @@ def trackMPPT(serial, Dprint):
         dprint(ps.mppt,'Serial port not specified.')
         return (True,0)
     pp = powerPointTrack()
+    prepareDevice(serial)
+
+    #
     # Set the current to 0
     current = 0
     while current < 9000 :
         if setCurrent(serial,current):
             return (True,0)
+        getPower(serial)
         current += 1
 
 
