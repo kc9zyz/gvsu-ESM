e263727a48f327890f118d4c00056334ece55f52
Billy Neuson <bneuson@gmail.com>
2016-06-11 00:34:48 -0400

    Added ADC driver file, aded public key for use with website

---
+++ esmADC.py
@@ -0,0 +1,11 @@
+# Handle communication between ADC and Raspberry Pi
+
+# import raspberry pi i2c
+
+class esmADC:
+    def readChannel(self,num):
+        # Read the voltage of an ADC channel.
+        return
+    def __init__(self):
+        # Initialize ADC board
+        return
