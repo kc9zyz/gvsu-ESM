e483ec23008660cc0cbe358e9610972d34e234e3
Billy Neuson <bneuson@gmail.com>
2016-06-14 23:53:59 -0400

    Added web interface. esm.py now completes a data aquisition, and pushes result to the server

--- esm.py
+++ esm.py
@@ -1,13 +1,24 @@
 import esmSerial
 import esmDCLoad
 import esmPrint
+import esmWebInterface
 
 def serCallback(byte):
     print(str(byte,'ascii'), end="")
+
 dc = esmDCLoad.esmDCLoad()
 serPorts = [(esmSerial.esmSerialPorts.electronicLoad,"/dev/tty.usbserial",dc.getCallback(),38400)]
 p = esmPrint.esmPrint(False)
 s = esmSerial.esmSerial(p,serPorts)
-dc.trackMPPT(s,p,9000)
+output = dc.trackMPPT(s,p,9000)
+
+dataPoint = esmWebInterface.esmDataPoint()
+dataPoint.output = output
+
+webInterface = esmWebInterface.esmWebInterface('http://cis.gvsu.edu/~neusonw/solar/data/',b'AAAAB3NzaC1yc2EAAAADAQABAAABAQD9KNADbe8h+5ZZnT/mggSUPxvkmEmlT1rWJqwK4DdIb4d19HPUKu8OIhouYz4RepmBs3G3/JXRfDKGpSeNOYlOeXhUe8MRffXfV2ZaP819gqiuFha9wsvyWXLfO9f/GNfmAuc8r4FCfP4A77l/FU9tpT+fOxeP6al08iJ5Ua1fMFGf3hhqnpixanLgyylFD+pPjX6KXqczICUDTWwGmmsgyLTyUEmmUN4sW7WZc4fNaGQSKidsDDzMLE7dFbDVY8F/to//bihpI4UmLwJYK8D/S2OcvK3skgqLDJ5K1FWhHCzNIrcp9KVxx3Na2XgrukJzhU0eHHASuPfcYElKHK/v')
+
+webInterface.sendUpdate(dataPoint)
+
+
 s.close()
 
---
+++ esmWebInterface.py
@@ -0,0 +1,41 @@
+import requests
+import time
+from collections import namedtuple
+import json
+import os
+import base64
+import hashlib
+
+class esmDataPoint:
+    def __init__(self):
+        self.output = 0
+        self.timestamp = int(time.time())
+        self.loc = (0,0)
+
+class esmWebInterface:
+    def __init__(self, host, key):
+        self.url = host
+        self.key = key
+    def flushBacklog(self):
+        # Empty the backlog of unfinshed requests
+        pass
+    def sendUpdate(self, dataPoint):
+        # Add the random data to the data point
+        dataPoint.random = base64.b64encode(os.urandom(12)).decode('ascii')
+        # Create the json string
+        dataBytes = str.encode(json.dumps(dataPoint.__dict__))
+        # Create a hash of the data and key
+        m = hashlib.md5()
+        m.update(dataBytes)
+        m.update(self.key)
+        # Configure the post body payload
+        payload = {'json': dataBytes, 'hash': m.hexdigest()}
+        # Sen the post request
+        r = requests.post(self.url + '?asset=current-data',data = payload)
+        Print the result
+        print(r.status_code)
+        print(r.text)
+
+
+
+
