67c86e896e45b626f7c6251f5a874fa5d9b2362b
Billy Neuson <bneuson@gmail.com>
2016-06-22 00:08:50 -0400

    Fixed smbus mock to add extra functions used.

--- esmMocksmbus.py
+++ esmMocksmbus.py
@@ -3,5 +3,9 @@ class SMBus:
         pass
     def write_byte(*args, **kw):
         pass
+    def write_byte_data(*args, **kw):
+        pass
     def read_byte(*args, **kw):
+        return 0x20
+    def read_byte_data(*args, **kw):
         return 0x20
