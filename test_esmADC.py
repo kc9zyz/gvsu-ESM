b7b17bb18a3e67b04e7820194c26d6875be64d5c
Billy Neuson <bneuson@gmail.com>
2016-06-26 21:09:59 +0000

    Updated ADC test, added 1wire output file

--- test_esmADC.py
+++ test_esmADC.py
@@ -6,7 +6,9 @@ class TestSerialMethods(unittest.TestCase):
     s = None
     def test_init(self):
         adc = esmADC.esmADC()
-        self.assertIsNotNone(adc.read())
+        a = adc.read()
+        self.assertIsNotNone(a)
+        print(a)
 
 if __name__ == '__main__':
     unittest.main()
