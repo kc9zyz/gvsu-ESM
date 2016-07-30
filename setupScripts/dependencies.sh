e599cbe4e109911cf2323e62166987bc0f94996a
Billy Neuson <bneuson@gmail.com>
2016-07-30 19:01:29 -0400

    Added pm2 setup to scripts

--- setupScripts/dependencies.sh
+++ setupScripts/dependencies.sh
@@ -1,4 +1,8 @@
 # Run apt-get to get the dependencies required by the project
-sudo apt-get update
-sudo apt-get install python3-rpi.gpio python3-smbus i2c-tools libi2c-dev build-essential python-dev git scons swig
+apt-get update
+apt-get install -y python3-rpi.gpio python3-smbus i2c-tools libi2c-dev build-essential python-dev git scons swig nodejs npm
+npm install -g npm
+npm install -g pm2
+npm install -g n
+n stable
 
--- setupScripts/setupPython.sh
+++ setupScripts/setupPython.sh
@@ -5,4 +5,7 @@ git clone https://github.com/jgarff/rpi_ws281x.git
 cd rpi_ws281x
 scons
 cd python
-sudo python setup.py install
+python setup.py install
+cd ../..
+
+pm2 start esm.py --interpreter=myenv/bin/python
